85d0b31c986376a97a97e8a739c2b9c0a5356c23
billysandey <billysandey@gmail.com>
2016-03-18 22:08:38 -0300

    - Realizado refatoração no código

--- app/src/main/java/br/com/app/videoaulasandroid/ListaDeAulasActivity.java
+++ app/src/main/java/br/com/app/videoaulasandroid/ListaDeAulasActivity.java
@@ -35,50 +35,11 @@ public class ListaDeAulasActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent i = new Intent(ListaDeAulasActivity.this, AulasActivity.class);
                 Bundle params = new Bundle();
-                switch (position){
-                    case 0:
-                        params.putString("ID_VIDEO", "stmPydPcDAQ");
-                        params.putString("IMG", "http://img.youtube.com/vi/stmPydPcDAQ/0.jpg");
-                        params.putString("TITLE", video.getTitleVideo()[position]);
-                        params.putString("SUB_TITLE", video.getSubTitle()[position]);
-                        i.putExtras(params);
-                        startActivity(i);
-                        break;
-                    case 1:
-                        params.putString("ID_VIDEO", "oBHrtUrq9p0");
-                        params.putString("IMG", "http://img.youtube.com/vi/oBHrtUrq9p0/0.jpg");
-                        params.putString("TITLE", video.getTitleVideo()[position]);
-                        params.putString("SUB_TITLE", video.getSubTitle()[position]);
-                        i.putExtras(params);
-                        startActivity(i);
-                        break;
-                    case 2:
-                        params.putString("ID_VIDEO", "MKHffejfugQ");
-                        params.putString("IMG", "http://img.youtube.com/vi/MKHffejfugQ/0.jpg");
-                        params.putString("TITLE", video.getTitleVideo()[position]);
-                        params.putString("SUB_TITLE", video.getSubTitle()[position]);
-                        i.putExtras(params);
-                        startActivity(i);
-                        break;
-                    case 3:
-                        params.putString("ID_VIDEO", "CBLDvBLoE6w");
-                        params.putString("IMG", "http://img.youtube.com/vi/CBLDvBLoE6w/0.jpg");
-                        params.putString("TITLE", video.getTitleVideo()[position]);
-                        params.putString("SUB_TITLE", video.getSubTitle()[position]);
-                        i.putExtras(params);
-                        startActivity(i);
-                        break;
-                    case 4:
-                        params.putString("ID_VIDEO", "thC3dezbKXs");
-                        params.putString("IMG", "http://img.youtube.com/vi/thC3dezbKXs/0.jpg");
-                        params.putString("TITLE", video.getTitleVideo()[position]);
-                        params.putString("SUB_TITLE", video.getSubTitle()[position]);
-                        i.putExtras(params);
-                        startActivity(i);
-                        break;
-                    default:
-                        break;
-                }
+                params.putString("ID_VIDEO", video.getKeyTube()[position]);
+                params.putString("TITLE", video.getTitleVideo()[position]);
+                params.putString("SUB_TITLE", video.getSubTitle()[position]);
+                i.putExtras(params);
+                startActivity(i);
             }
         });
     }
--- app/src/main/java/br/com/app/videoaulasandroid/Video.java
+++ app/src/main/java/br/com/app/videoaulasandroid/Video.java
@@ -6,9 +6,10 @@ package br.com.app.videoaulasandroid;
 public class Video {
 
     private String[] titleVideo;
-    public String[] subTitle;
-    public String[] duration;
-    public Integer[] imagem;
+    private String[] subTitle;
+    private String[] duration;
+    private String[] keyTube;
+    private Integer[] imagem;
 
     public Video(){
         this.titleVideo = new String[]{
@@ -16,7 +17,22 @@ public class Video {
                 "Curso de Android - Aula 2",
                 "Curso de Android - Aula 3",
                 "Curso de Android - Aula 4",
-                "Curso de Android - Aula 5"
+                "Curso de Android - Aula 5",
+                "Curso de Android - Aula 6",
+                "Curso de Android - Aula 7",
+                "Curso de Android - Aula 8",
+                "Curso de Android - Aula 9",
+                "Curso de Android - Aula 10",
+                "Curso de Android - Aula 11",
+                "Curso de Android - Aula 12",
+                "Curso de Android - Aula 13",
+                "Curso de Android - Aula 14",
+                "Curso de Android - Aula 15",
+                "Curso de Android - Aula 16",
+                "Curso de Android - Aula 17",
+                "Curso de Android - Aula 18",
+                "Curso de Android - Aula 19",
+                "Curso de Android - Aula 20"
         };
 
         this.subTitle = new String[]{
@@ -24,7 +40,55 @@ public class Video {
                 "Primeiro Projeto",
                 "Emulador",
                 "Estrutura de Pastas",
-                "Activity"
+                "Activity, View, LogCat - parte 1",
+                "Activity, View, LogCat - parte 2",
+                "XML",
+                "Gerenciadores de Layout I",
+                "Gerenciadores de Layout II",
+                "Gerenciadores de Layout III",
+                "LinearLayout II",
+                "Gerenciadores de Layout",
+                "LinearLayout IV",
+                "Calculadora",
+                "Calculadora II",
+                "Calculadora III",
+                "Calculadora IV",
+                "string.xm",
+                "Toast ",
+                "AndroidManifest.xm",
+                "Activity I",
+                "Activity II",
+                "Classe R",
+                "Menu",
+                "Activity & Menu",
+        };
+
+        this.keyTube = new String[]{
+                "stmPydPcDAQ",
+                "oBHrtUrq9p0",
+                "MKHffejfugQ",
+                "CBLDvBLoE6w",
+                "thC3dezbKXs",
+                "CisDKrxYP9M",
+                "aB5-Kb29U2Q",
+                "3Gfx7tH6rQY",
+                "e6xsAVfV3zo",
+                "DlPIn5FDl_Y",
+                "VR2O6PpEYhA",
+                "kgZEm3Wns4E",
+                "IZ0wRgf8EAc",
+                "z8z1ZtqU60Q",
+                "SUYWYHZvoqo",
+                "4XgWcO_tapo",
+                "U_D7rxDRGNw",
+                "0eHrYm7AG54",
+                "0QI3cSP9zi4",
+                "gQFq3HFh8qY",
+                "iqTQBRrs5E4",
+                "w046yN15XeI",
+                "MEegto1_GpM",
+                "Kz_JqWukVGw",
+                "kRYHHyJk44k"
         };
 
         this.duration = new String[]{
@@ -32,10 +96,50 @@ public class Video {
                 "4:53",
                 "7:22",
                 "4:18",
-                "6:34"
+                "6:34",
+                "6:08",
+                "5:58",
+                "3:57",
+                "4:12",
+                "9:12",
+                "6:55",
+                "5:27",
+                "6:59",
+                "6:54",
+                "6:38",
+                "4:45",
+                "5:34",
+                "5:58",
+                "6:45",
+                "6:32",
+                "9:02",
+                "5:09",
+                "5:01",
+                "6:16",
+                "7:25",
         };
 
         this.imagem = new Integer[]{
+                R.drawable.icon_video,
+                R.drawable.icon_video,
+                R.drawable.icon_video,
+                R.drawable.icon_video,
+                R.drawable.icon_video,
+                R.drawable.icon_video,
+                R.drawable.icon_video,
+                R.drawable.icon_video,
+                R.drawable.icon_video,
+                R.drawable.icon_video,
+                R.drawable.icon_video,
+                R.drawable.icon_video,
+                R.drawable.icon_video,
+                R.drawable.icon_video,
+                R.drawable.icon_video,
+                R.drawable.icon_video,
+                R.drawable.icon_video,
+                R.drawable.icon_video,
+                R.drawable.icon_video,
+                R.drawable.icon_video,
                 R.drawable.icon_video,
                 R.drawable.icon_video,
                 R.drawable.icon_video,
@@ -77,4 +181,11 @@ public class Video {
         return imagem;
     }
 
+    public String[] getKeyTube() {
+        return keyTube;
+    }
+
+    public void setKeyTube(String[] keyTube) {
+        this.keyTube = keyTube;
+    }
 }
